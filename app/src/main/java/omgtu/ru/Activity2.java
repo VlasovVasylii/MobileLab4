71c83eb861a976372de04284c025f482548f7d24
VlasovVasylii <dedkek56@gmail.com>
2026-02-28 18:46:56 +0600

    feature: add name input in MainActivity and display it in Activity2 via Intent

---
+++ app/src/main/java/omgtu/ru/Activity2.java
@@ -0,0 +1,32 @@
+package omgtu.ru;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main2), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        TextView textViewDisplayName = findViewById(R.id.textViewDisplayName);
+        
+        String userName = getIntent().getStringExtra("USER_NAME");
+        if (userName != null) {
+            textViewDisplayName.setText(userName);
+        }
+    }
+}
--- app/src/main/java/omgtu/ru/MainActivity.java
+++ app/src/main/java/omgtu/ru/MainActivity.java
@@ -1,6 +1,9 @@
 package omgtu.ru;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        Button buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(v -> {
+            String name = editTextName.getText().toString();
+            Intent intent = new Intent(MainActivity.this, Activity2.class);
+            intent.putExtra("USER_NAME", name);
+            startActivity(intent);
         });
     }
 }
